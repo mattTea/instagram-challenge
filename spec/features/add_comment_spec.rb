1318fcea1ea4fe27bae6de2eff33f6626887c6df
Matt Thompson <matt.thompson@johnlewis.co.uk>
2019-04-29 09:41:17 +0100

    Adds number fo comments per post to timeline
    User story 6: See number of comments on timeline
    
    As a user,
    so that I measure the popularity of my pictures,
    I want to be able to see the number of comments added in the timeline view.

--- spec/features/add_comment_spec.rb
+++ spec/features/add_comment_spec.rb
@@ -17,4 +17,23 @@ RSpec.feature "Add comment", type: :feature do
     click_button "Create Comment"
     expect(page).to have_content "Author: mattTea"
   end
+
+  scenario "user can see number of comments for a post from the timeline" do
+    # create test post
+    visit "/pictures/new"
+    fill_in "Title", with: "First sham pic"
+    attach_file("picture_image", Rails.root + "spec/fixtures/picture.png")
+    click_button "Save Picture"
+    # return to list of posts
+    visit "/pictures"
+    # visit individual picture page
+    find("a[href='/pictures/1']").click
+    # add comment
+    fill_in "Author", with: "mattTea"
+    fill_in "Body", with: "Great first post!"
+    click_button "Create Comment"
+    # see number of comments for pic in timeline
+    visit "/pictures"
+    expect(page).to have_content "1 comment"
+  end
 end
